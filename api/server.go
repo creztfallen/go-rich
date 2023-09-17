cfa54684daa72d778318ba57efcccaff028caca0
Mateus Moreno <mtssmoreno@gmail.com>
2023-09-17 18:19:30 -0300

    feat: add latest exchange rate handler

--- api/server.go
+++ api/server.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"fmt"
+	"github.com/creztfallen/go-rich/api/handlers"
+	"net/http"
+)
+
+func StartServer() {
+	http.HandleFunc("/latest", handlers.LatestExchangeRateHandler)
+	http.HandleFunc("/historical", handlers.HistoricalExchangeRateHandler)
+
+	port := "8080"
+
+	fmt.Printf("Starting server on port %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
+}
